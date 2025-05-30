68ef8785504189715b3199261e1b78ab9a238466
liuxinlin <14649409+liuxinlin2@user.noreply.gitee.com>
2025-05-30 14:40:22 +0800

    star2

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // 添加这一行：配置生产环境的基础路径
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/liuxinlin/' // 替换为你的 GitHub 仓库名称（小写）
+    : '/'
 })
